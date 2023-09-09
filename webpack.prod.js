8c2870b761cd3b563abcf75867e79d02841e0622
Екатерина <polskaya007@mail.ru>
2023-09-09 11:59:20 +0300

    correct webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,13 @@
 const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    minimizer: [new TerserPlugin({}), new CssMinimizerWebpackPlugin({})],
   },
 });
 
